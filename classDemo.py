f18b09a2448cfc2fb3a50ba8bd5add868b48635d
Oluwatofunmi Oyetan <tofunmi.oyetan@uky.edu>
2021-07-15 17:35:39 +0000

    updated changes

--- classDemo.py
+++ classDemo.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, url_for, flash, redirect
-# from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm
 # from wtforms import StringField, PasswordField, SubmitField, BooleanField
-# from wtforms.validators import DataRequired, Length, Email, EqualTo
+# from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from forms import RegistrationForm
+from loginForms import LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from audio import printWAV
 import time, random, threading
 from turbo_flask import Turbo
 from flask_bcrypt import Bcrypt
+from flask_behind_proxy import FlaskBehindProxy
 
 
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+proxied = FlaskBehindProxy(app)
 bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = '21d5e83a343bebf7c4174dd694091ffd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #location in flask. when you run it create site.db file
@@ -47,8 +50,7 @@ def register():
     if form.validate_on_submit(): # checks if entries are valid
         pw_hash = bcrypt.generate_password_hash(password=form.password.data)
         bcrypt.check_password_hash(pw_hash, password=form.password.data)
-        print(pw_hash)
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=pw_hash)
         print(user)
         db.session.add(user)
         db.session.commit()
@@ -60,13 +62,14 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit(): 
-        pw_hash = bcrypt.generate_password_hash(password=form.password.data)
-        bcrypt.check_password_hash(pw_hash, password=form.password.data)
-        print(pw_hash)
-        user = User(username=form.username.data, password=form.password.data)
-        print(user)
-        db.session.add(user)
-        db.session.commit()
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            flash(f'Incorrect username for {form.username.data}! Try again!', 'failed')
+            return redirect(url_for('login'))
+        password = User.query.filter_by(password=form.password.data).first()
+        if not password:
+          flash(f'Incorrect password for {form.username.data}! Try again!', 'failed')
+          return redirect(url_for('login'))
         flash(f'Successful login for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('login.html', title='Login', form=form)
@@ -84,16 +87,27 @@ def before_first_request():
     file.close()
 
     #starting thread that will time updates
-    threading.Thread(target=update_captions).start()
+    threading.Thread(target=update_captions, daemon=True).start()
+    
+# integration testing    
+# def time_stamp():
+#     pass
+#     file = open("pos.txt","w")
+#     pos = int(file.read())
+#     file.write(str(pos+interval))
+#     file.close()
 
 @app.context_processor
 def inject_load():
     # getting previous time stamp
+#     try:
     file = open("pos.txt","r")
+#     except:  
     pos = int(file.read())
     file.close()
-
-    # writing next time stamp
+    
+#     time_stamp()
+#     writing next time stamp
     file = open("pos.txt","w")
     file.write(str(pos+interval))
     file.close()
@@ -112,4 +126,4 @@ def update_captions():
             
 
 if __name__ == '__main__':               # this should always be at the end
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")  #5f788a
--- loginForms.py
+++ loginForms.py
@@ -1,7 +1,8 @@
 #import some basics
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
 
 
 #Create some fields
@@ -9,4 +10,16 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class LoginForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
   password = PasswordField('Password', validators=[DataRequired()])
-  submit = SubmitField('Sign Up')     
+  submit = SubmitField('Sign Up')   
+#   def check(username):
+#     user = User.query.filter_by(username = username).first()
+#     if not user:
+#       raise ValidationError(user)
+      
+#   def check(password):
+#     password = User.query.filter_by(password = password).first()
+#     if not password:
+#       raise ValidationError(password)
+      
+  
+      
